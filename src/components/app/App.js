a70126c52a402d4a785600ad7619edb0e15e7521
GrimdvlLaptop <grimdvl@gmail.com>
2024-11-09 23:54:15 +0200

    lesson 23 homework done

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import PropTypes from 'prop-types';
 import AppHeader from "../appHeader/AppHeader";
-import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages'
+import {MainPage, ComicsPage, Page404, SinglePage} from '../pages';
+import SingleComicLayout from "../pages/singleComicLayout/SingleComicLayout";
+import SingleCharacterLayout from "../pages/singleCharacterLayout/SingleCharacterLayout";
 
 const App = () => {
     return (
@@ -13,7 +15,14 @@ const App = () => {
                     <Routes>
                         <Route path="/" element={<MainPage/>}/>
                         <Route path="/comics" element={<ComicsPage/>}/>
-                        <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                        <Route 
+                            path="/comics/:id" 
+                            element={<SinglePage Component={SingleComicLayout} dataType="comic"/>}
+                        />
+                        <Route 
+                            path="/characters/:id" 
+                            element={<SinglePage Component={SingleCharacterLayout} dataType="character"/>}
+                        />
                         <Route path="*" element={<Page404/>}/>
                     </Routes>
                 </main>
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,6 +1,8 @@
 import MainPage from "./MainPage";
 import ComicsPage from "./ComicsPage";
 import Page404 from "./404";
-import SingleComicPage from "./SingleComicPage";
+// import SingleComicPage from "./SingleComicPage";
+import SinglePage from "./SinglePage";
 
-export {MainPage, ComicsPage, Page404, SingleComicPage};
+// export {MainPage, ComicsPage, Page404, SingleComicPage};
+export {MainPage, ComicsPage, Page404, SinglePage};
